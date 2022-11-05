f0858be65dad414c0d593bedca1ebd65feb2e257
Altaev Nurzhan <38946653+ksixen@users.noreply.github.com>
2022-11-05 20:38:56 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./main.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="typing-anim">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="jumping-anim">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="resizing-anim">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
